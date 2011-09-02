be9062b0a9ce0fc101555d514d29bb4be45bdaba
neelimas <sneelima123@gmail.com>
2011-09-02 15:06:01 -0500

    Allows devs to check the code quality before checking in the code.

---
+++ code_watch_local.rb
@@ -0,0 +1,12 @@
+require "git_history"
+
+class CodeWatchLocal
+  def initialize
+    if GitHistory.new.pass? :
+      puts "pass"
+    else
+      puts  "Your code does not have adequate tests."
+    end
+  end
+  CodeWatchLocal.new()
+end
